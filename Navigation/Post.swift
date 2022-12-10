75dd4c1106507d21af1eb2bd19135f7ba52544eb
Sergey Topilsky <s.g.topilsky@gmail.com>
2022-12-10 23:03:59 +0300

    Корректировки в соответствии с рекомендациями А. Павлова

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -14,3 +14,12 @@ struct Post {
     let likes: Int
     let views: Int
 }
+
+let arrayOfPosts: [Post] = [
+    Post(author: "Анри Матисс", description: "Танец - год создания 1910", image: "Painting1", likes: 1950, views: 20000),
+    Post(author: "Марк Захарович Шагал", description: "Пролетая над городом - год создания 1918", image: "Painting2", likes: 907, views: 1900),
+    Post(author: "Винсент Ван Гог", description: "Звездная ночь - год создания 1889", image: "Painting3", likes: 12309, views: 70000),
+    Post(author: "Каземир Малевич", description: "Голова крестьянина - год создания 1929", image: "Painting4", likes: 1950, views: 20000),
+    Post(author: "Пабло Пикассо", description: "Девочка на шаре - год создания 1905", image: "Painting5", likes: 7, views: 15),
+    Post(author: "Сальвадор Дали", description: "Постоянство памяти - год создания 1931", image: "Painting6", likes: 99999, views: 999999),
+]
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,36 +9,21 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    private lazy var profileHeaderView: ProfileHeaderView = {
-        let profileHeaderView = ProfileHeaderView(frame: .zero)
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        return profileHeaderView
-    }()
-
     private lazy var tableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultTableViewCell")
         tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "CustomTableViewCell")
+        tableView.register(ProfileHeaderView.self, forHeaderFooterViewReuseIdentifier: "ProfileHeaderView")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-
-    private let arrayOfPosts: [Post] = [
-        Post(author: "Анри Матисс", description: "Танец - год создания 1910", image: "Painting1", likes: 1950, views: 20000),
-        Post(author: "Марк Захарович Шагал", description: "Пролетая над городом - год создания 1918", image: "Painting2", likes: 907, views: 1900),
-        Post(author: "Винсент Ван Гог", description: "Звездная ночь - год создания 1889", image: "Painting3", likes: 12309, views: 70000),
-        Post(author: "Каземир Малевич", description: "Голова крестьянина - год создания 1929", image: "Painting4", likes: 1950, views: 20000),
-        Post(author: "Пабло Пикассо", description: "Девочка на шаре - год создания 1905", image: "Painting5", likes: 7, views: 15),
-        Post(author: "Сальвадор Дали", description: "Постоянство памяти - год создания 1931", image: "Painting6", likes: 99999, views: 999999),
-    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemGray6
         self.navigationController?.navigationBar.isHidden = false
-        self.view.addSubview(self.profileHeaderView)
-        self.navigationItem.title = profileHeaderView.profileTitle
         self.view.addSubview(self.tableView)
         self.constraintsActivating()
     }
@@ -46,12 +31,7 @@ class ProfileViewController: UIViewController {
 
     private func constraintsActivating () {
         NSLayoutConstraint.activate([
-            self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
-
-            self.tableView.topAnchor.constraint(equalTo: self.profileHeaderView.bottomAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
             self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
@@ -74,12 +54,25 @@ extension ProfileViewController: UITableViewDataSource {
             return cell
         }
         
-        let post = self.arrayOfPosts[indexPath.row]
+        let post = arrayOfPosts[indexPath.row]
         cell.setup(author: post.author,
                    description: post.description,
                    image: post.image,
                    likes: post.likes,
                    views: post.views)
         return cell
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "ProfileHeaderView") as! ProfileHeaderView
+        view.backgroundColor = .systemBlue
+        return view
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section == 0 ? 200 : 0
     }
 }
